134eecc81f0d35c0a7a165bdae93b81ebe93b15d
Ian Sinnott <ian@iansinnott.com>
2016-02-13 17:21:52 -0800

    Implement counter

--- exercises/counter/client/App.js
+++ exercises/counter/client/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import 'normalize.css';
 
 // Import CSS and favicon
 import './App.css';
 import './favicon.ico';
 
+const Counter = React.createClass({
+  propTypes: {
+    count: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired,
+  },
+
+  render() {
+    const { count, onIncrement, onDecrement } = this.props;
+    return (
+      <div className='Counter'>
+        <div className='siteTitle'>
+          <h1>{count}</h1>
+        </div>
+        <div className='controls'>
+          <button onClick={onDecrement}>-</button>
+          <button onClick={onIncrement}>+</button>
+        </div>
+      </div>
+    );
+  },
+});
+
 export const App = React.createClass({
+  getInitialState() {
+    return {
+      count: 0,
+    };
+  },
+  onIncrement() {
+    const { count } = this.state;
+    this.setState({ count: count + 1 });
+  },
+  onDecrement() {
+    const { count } = this.state;
+    this.setState({ count: count - 1 });
+  },
   render() {
     return (
       <div className='App'>
-        <img src='https://facebook.github.io/react/img/logo.svg' alt='React Logo' />
-        <p>Big wins</p>
+        <Counter
+          onIncrement={this.onIncrement}
+          onDecrement={this.onDecrement}
+          {...this.state} />
       </div>
     );
   },
