ecdd547e89e20e9fca7a4be655cd581b94471e48
Ian Sinnott <ian@iansinnott.com>
2016-02-15 12:59:12 -0800

    Add initial reddit-fetch-2 exercise files

---
+++ exercises/reddit-fetch-2/client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import the App component
+import { App } from './App.js';
+
+// Render the App to the DOM at the <div id='root'></div> element
+render(<App />, document.getElementById('root'));
