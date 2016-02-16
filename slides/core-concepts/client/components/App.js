3694ad87208b60ffe7e9ac7a6253f7ee55a8d16f
Ian Sinnott <ian@iansinnott.com>
2016-02-15 17:35:05 -0800

    Add more key events

--- slides/core-concepts/client/components/App.js
+++ slides/core-concepts/client/components/App.js
@@ -1,55 +1,13 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink, History } from 'react-router';
+import { Link, IndexLink, History, Location } from 'react-router';
+import toNumber from 'lodash/toNumber';
 import 'normalize.css';
 
 // Using CSS Modules so we assign the styles to a variable
 import s from './App.styl';
-import logo from './react-logo.png';
 
 // Favicon link is in the template, this just makes webpack package it up for us
 import './favicon.ico';
-
-const Counter = React.createClass({
-  propTypes: {
-    count: PropTypes.number.isRequired,
-    onIncrement: PropTypes.func.isRequired,
-    onDecrement: PropTypes.func.isRequired,
-  },
-
-  render() {
-    const { count, onIncrement, onDecrement } = this.props;
-    return (
-      <div className={s.Counter}>
-        <div className={s.siteTitle}>
-          <h1>{count}</h1>
-        </div>
-        <div className={s.controls}>
-          <button onClick={onDecrement}>-</button>
-          <button onClick={onIncrement}>+</button>
-        </div>
-      </div>
-    );
-  },
-});
-
-export const Home = React.createClass({
-  onIncrement() {
-    this.setState({ count: count + 1 });
-  },
-  onDecrement() {
-    this.setState({ count: count - 1 });
-  },
-  render() {
-    return (
-      <div className={s.page}>
-        <Counter
-          onIncrement={this.onIncrement}
-          onDecrement={this.onDecrement}
-          {...this.state} />
-      </div>
-    );
-  },
-});
 
 export const NotFound = React.createClass({
   render() {
@@ -67,10 +25,12 @@ export const NotFound = React.createClass({
  * still use createClass here.
  */
 export const App = React.createClass({
-  mixins: [History],
+  mixins: [History, Location],
 
   propTypes: {
     children: PropTypes.any,
+    history: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
   },
 
   componentDidMount() {
@@ -83,39 +43,40 @@ export const App = React.createClass({
 
   onNext() {
     // TODO:
-    // 1. Get current slide number
-    // 2. Add one to it
-    // 3. Call this.history.pushState appropriately
-    this.history.goForward();
+    // Do not call if we are on the last slide
+    const next = toNumber(this.props.location.pathname.slice(1)) + 1;
+    this.props.history.pushState(null, `/${next}`);
   },
 
   onPrev() {
-    // TODO:
-    // 1. Check if there is a pervious slide
-    // 2. If so then go to it
-    this.history.goBack();
+    const prev = toNumber(this.props.location.pathname.slice(1)) - 1;
+    if (prev < 1) return;
+    this.props.history.pushState(null, `/${prev}`);
+  },
+
+  toggleFullscreen() {
+    // TODO
+    console.log('Toggling full screen');
   },
 
   handleKeyDown(e) {
     const LEFT = 37;
     const RIGHT = 39;
+    const F = 70;
 
-    if (e.which === LEFT) {
+    if (e.which === LEFT)
       return this.onPrev();
-    } else if (e.which === RIGHT) {
+    else if (e.which === RIGHT)
       return this.onNext();
-    } else {
+    else if (e.which === F && e.shiftKey && !e.metaKey && !e.ctrlKey)
+      return this.toggleFullscreen();
+    else
       return; // Do nothing
-    }
   },
 
   render() {
     return (
       <div className={s.App}>
-        <nav className={s.nav}>
-          <IndexLink to='/' activeClassName={s.active}>Home</IndexLink>
-          <Link to='/about' activeClassName={s.active}>About</Link>
-        </nav>
         {this.props.children}
       </div>
     );
