9b4d0e2ecfa05b76479cde9ee633a30964330340
Ian Sinnott <ian@iansinnott.com>
2016-02-17 09:13:05 -0800

    Automatically build slide rouets from slides dir

--- slides/core-concepts/client/routes.js
+++ slides/core-concepts/client/routes.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Route, IndexRedirect } from 'react-router';
+import toString from 'lodash/toString';
 
 import { App, NotFound } from './components/App.js';
 
-import Intro from './slides/1-intro.js';
-import About from './slides/2-about.js';
-import FurtherReading from './slides/3-further-reading.js';
-
 // Need a list of all slides
+const context = require.context('./slides', false, /\.js$/);
+const slides = context.keys().sort().map(context);
 
 export const routes = (
   <Route path='/' title='App' component={App}>
     <IndexRedirect to='1' />
-    <Route path='1' component={Intro} />
-    <Route path='2' component={About} />
-    <Route path='3' component={FurtherReading} />
+    {slides.map((module, i) => {
+      const Component = module.__esModule ? module.default : module;
+      return <Route key={i} path={toString(i + 1)} component={Component} />;
+    })}
     <Route path='*' title='404: Not Found' component={NotFound} />
   </Route>
 );
---
+++ slides/core-concepts/client/slides/04-conclusion.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+
+/**
+ * @module Conclusion
+ */
+export const Conclusion = React.createClass({
+  render() {
+    return (
+      <div>
+        <span className='temp'>Conclusion</span>
+      </div>
+    );
+  },
+});
+
+export default Conclusion;
